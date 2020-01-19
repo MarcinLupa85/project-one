f07ae28164fdd0554d092441f3583790f3b8580c
Wiktor Marchewka <wiktormarchewka@gmail.com>
2020-01-19 20:08:55 +0100

    Updated run script with files ownership part

--- run_tests.sh
+++ run_tests.sh
@@ -6,3 +6,10 @@ mkdir -p report
 gradle clean test -i -Pdriver_type="$browser" -Pbuild_tag=$tag
 
 echo "END"
+
+if [[ -f /.dockerenv ]]; then
+    useradd -ms /bin/bash jenkins;
+    chown -R jenkins:jenkins gradleCache
+    chown -R jenkins:jenkins report
+    chown -R jenkins:jenkins build
+fi
