8f35970751b019ac231b1c2fd7eb1d94b9fe0fea
Michal Sepczuk <faunt@o2.pl>
2020-03-11 13:47:07 +0100

    Adding new page objects and new page operations

--- src/test/java/ContactWithCircleKTests.java
+++ src/test/java/ContactWithCircleKTests.java
@@ -6,17 +6,17 @@ import org.testng.annotations.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ContactWithCircleKTests extends TestsBase {
-    /*private HomePageOperations homePageOperations;
-
-    @BeforeClass
-    private void initOperations() {
-        homePageOperations = new HomePageOperations(driver);
-    }
-
-    @Test
-    public void testOpenContactSection() {
-        //homePageOperations.openContactSection();
-        assertThat(driver.getCurrentUrl()).contains("/kontakt");
-    }*/
+//    private HomePageOperations homePageOperations;
+//
+//    @BeforeClass
+//    private void initOperations() {
+//        homePageOperations = new HomePageOperations(driver);
+//    }
+//
+//    @Test
+//    public void testOpenContactSection() {
+//        //homePageOperations.openContactSection();
+//        assertThat(driver.getCurrentUrl()).contains("/kontakt");
+//    }
 
 }
