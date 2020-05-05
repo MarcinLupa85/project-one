421798f24c3d738653e25a96a71101ac8094c342
krystian.kowalski <krykow@statoilfuelretail.com>
2020-05-05 13:14:33 +0200

    Crated Mailinator component

---
+++ src/main/java/operations/MailinatorPageOperations.java
@@ -0,0 +1,39 @@
+package operations;
+
+import java.lang.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import pageobjects.MailinatorPageObject;
+import utils.FormUtils;
+import utils.WaitUtils;
+
+public class MailinatorPageOperations {
+    private MailinatorPageObject mailinatorPageObject;
+    private WebDriver webDriver;
+    private FormUtils formUtils;
+    private WaitUtils waitUtils;
+    long timeNow = System.currentTimeMillis();
+
+    public long getTimeNow() {
+        return timeNow;
+    }
+
+    public MailinatorPageOperations(WebDriver driver) {
+        mailinatorPageObject = new MailinatorPageObject(driver);
+        waitUtils = new WaitUtils(driver);
+        formUtils = new FormUtils(driver);
+        webDriver = driver;
+    }
+
+    public void checkMailinator(String email) {
+        String mailinatorPageURL = "https://www.mailinator.com/";
+        webDriver.navigate().to(mailinatorPageURL);
+        waitUtils.waitForElement(mailinatorPageObject.getEnterMailName());
+        formUtils.fillField(mailinatorPageObject.getEnterMailName(), email);
+        mailinatorPageObject.getEnterMailName().sendKeys(Keys.ENTER);
+        waitUtils.waitForElement(mailinatorPageObject.getMailCheckbox());
+        mailinatorPageObject.getMailCheckbox().click();
+    }
+
+
+}
---
+++ src/main/java/pageobjects/MailinatorPageObject.java
@@ -0,0 +1,22 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MailinatorPageObject {
+    public MailinatorPageObject(WebDriver driver)  {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = "#addOverlay")
+    private WebElement enterMailName;
+
+    @FindBy(css = "input[class='flat']")
+    private WebElement mailCheckbox;
+
+    public WebElement getEnterMailName() { return enterMailName; }
+
+    public WebElement getMailCheckbox() { return mailCheckbox; }
+}
