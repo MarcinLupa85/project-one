3bdce882c40d6fe176c4571f91b041b6cdb90e54
Wiktor Marchewka <wiktormarchewka@gmail.com>
2020-06-26 10:16:52 +0200

    Fixes after code review

--- src/main/java/date/DateFormatter.java
+++ src/main/java/date/DateFormatter.java
@@ -6,7 +6,7 @@ import static java.time.LocalDateTime.now;
 
 public class DateFormatter {
 
-    private static DateTimeFormatter timeStampPattern = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+    private static DateTimeFormatter timeStampPattern = DateTimeFormatter.ofPattern("ddMMyyyy_HH:mm:ss");
 
     public static String getFormattedNowDate() {
         return timeStampPattern.format(now());
