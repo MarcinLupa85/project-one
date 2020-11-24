6b0e461487025c22a5ac27d480e587dc371216c1
Krystian-Kowalski <krykow@statoilfuelretail.com>
2020-11-24 23:55:56 +0100

    Deleted comments

--- src/main/java/operations/ArticlesPageOperations.java
+++ src/main/java/operations/ArticlesPageOperations.java
@@ -18,30 +18,12 @@ public class ArticlesPageOperations {
     private ArticlesPageObject articlesPageObject;
     private WaitUtils waitUtils;
     private WebDriver driver;
-//    private  List<String> listOfArticles = new ArrayList<>();
 
     public ArticlesPageOperations(WebDriver driver) {
         articlesPageObject = new ArticlesPageObject(driver);
         waitUtils = new WaitUtils(driver);
         this.driver = driver;
-//        this.listOfArticles = listOfArticles;
     }
-
-//    private class Article {
-//        String articleTitle = articlesPageObject.getArticleTitle().getText();
-//        String articleCategory = articlesPageObject.getArticleCategory().getText();
-//    }
-//
-//    public List<String> listArticles(String category){
-//        List<Article> articles = new ArrayList<>();
-//        String categoryName = driver.findElement(By.className("article-tile__category")).getText();
-//        for (article : articles) {
-//            if(categoryName == category){
-//                articles.add(articleTitle);
-//            }
-//        }
-//    }
-
 
     public List<String> addToList(String category) throws TimeoutException {
         List<String> listOfArticles = new ArrayList<>();
@@ -70,38 +52,3 @@ public class ArticlesPageOperations {
         return listOfArticles.stream().limit(3).collect(Collectors.toList());
     }
 }
-
-
-//    private List<String> listThreeArticles(String category) throws TimeoutException {
-//        while (listOfArticles.size()<3) {
-//            waitUtils.waitForDocumentReadyState();
-//            List<String> listOfArticles = articlesPageObject.getArticleTitleCategoryList()
-//                    .stream()
-//                    .filter(webElement -> webElement.findElement(By.className("article-tile__category")).getText().contains(category))
-//                    .map(webElement -> webElement.findElement(By.className("article-tile__title")).getText())
-////                .limit(3)
-//                    .collect(Collectors.toList());
-//            articlesPageObject.getNextButton().click();
-//            System.out.println(listOfArticles);
-//            return listOfArticles;
-//        }
-//        return listOfArticles;
-//    }
-
-//    public List<String> addToList(String category) throws TimeoutException {
-//        while (listOfArticles.size()<3) {
-//            waitUtils.waitForDocumentReadyState();
-//            List<String> listOfArticles = articlesPageObject.getArticleTitleCategoryList()
-//                    .stream()
-//                    .filter(webElement -> webElement.findElement(By.className("article-tile__category")).getText().contains(category))
-//                    .map(webElement -> webElement.findElement(By.className("article-tile__title")).getText())
-////                .limit(3)
-//                    .collect(Collectors.toList());
-//            articlesPageObject.getNextButton().click();
-//            return listOfArticles;
-//        }
-//        System.out.println(listOfArticles);
-//        return listOfArticles;
-//        String abc = listOfArticles.get(0);
-//    }
-//}
