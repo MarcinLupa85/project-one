f2fb07cd09e3549d2f5b8915929b983571948331
Krystian-Kowalski <krykow@statoilfuelretail.com>
2021-03-30 07:55:18 +0200

    price list update

--- src/main/java/operations/CustomizationPageOperations.java
+++ src/main/java/operations/CustomizationPageOperations.java
@@ -59,38 +59,38 @@ public class CustomizationPageOperations {
     }
 
     public void goToSDUPartner() {
-        driver.navigate().to(BASE_URL + "bestill/Y21-74576");
+        driver.navigate().to(BASE_URL + "bestill/Y21-740505");
         waitUtils.waitForVisiblityOf(customizationPageObject.getSubmitButton());
     }
 
     public void goToMDULight() {
-        driver.navigate().to(BASE_URL + "bestill/Y21-785527");
+        driver.navigate().to(BASE_URL + "bestill/Y21-580541");
         waitUtils.waitForVisiblityOf(customizationPageObject.getSubmitButton());
     }
 
     public void goToMDUReady() {
-        driver.navigate().to(BASE_URL + "bestill/Y21-723430");
+        driver.navigate().to(BASE_URL + "bestill/Y21-18954");
         waitUtils.waitForVisiblityOf(customizationPageObject.getSubmitButton());
     }
 
     public void goToMDULeasing() {
-        driver.navigate().to(BASE_URL + "bestill/Y21-682527");
+        driver.navigate().to(BASE_URL + "bestill/Y21-761442");
         waitUtils.waitForVisiblityOf(customizationPageObject.getSubmitButton());
     }
 
     public void checkSDUPartnerPrices() {
-        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s6.895,-");
+        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s7.895,-");
     }
 
     public void checkMDULightPrices() {
-        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s19.990,-");
+        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s20.990,-");
     }
     public void checkMDUReadyPrices() {
-        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s16.990,-");
+        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s18.990,-");
     }
 
     public void checkMDULeasingPrices() {
-        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s199,-");
+        assertThat(customizationPageObject.getTotalPrice().getText()).containsPattern("\\s299,-");
     }
 
     public void fillMembershipNumber(String membershipNumber) {
