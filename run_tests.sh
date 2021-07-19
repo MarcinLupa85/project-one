5c38add5410164546210ba787e67d3955a4b17de
Wiktor Marchewka <wiktormarchewka@gmail.com>
2021-07-19 20:05:58 +0200

    Removed changing access modifiers from run script

--- run_tests.sh
+++ run_tests.sh
@@ -7,10 +7,3 @@ mkdir -p report
 
 echo "END"
 
-if [[ -f /.dockerenv ]]; then
-    echo "CHANGING ACCESS MODIFIERS"
-    chmod -R 777 gradleCache
-    chmod -R 777 report
-    chmod -R 777 build
-    chmod -R 777 .gradle
-fi
